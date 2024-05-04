b3262d66f3857a0ed5a2f1cedfab1245b12f2e05
TyroneMartin <tyrone306@byui.edu>
2024-05-04 14:49:42 -0600

    Lint error fixes made to cart.js to include arrow function

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,23 +4,16 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   let transformedCartItems;
 
-  // Check if cartItems is an array
   if (!Array.isArray(cartItems)) {
     // If cartItems is not an array, convert it to an array
     const cartItemsArray = cartItems ? [cartItems] : [];
     // Now you can use map() safely on cartItemsArray
-    transformedCartItems = cartItemsArray.map((item) => {
-      // Transformation logic
-      return cartItemTemplate(item);
-    });
+    transformedCartItems = cartItemsArray.map(item => cartItemTemplate(item));
   } else {
     // If cartItems is already an array, you can directly use map() on it
-    transformedCartItems = cartItems.map((item) => {
-      // Transformation logic
-      return cartItemTemplate(item);
-    });
+    transformedCartItems = cartItems.map(item => cartItemTemplate(item));
   }
-
+  
   // Set the innerHTML after mapping is done
   document.querySelector(".product-list").innerHTML = transformedCartItems.join("");
 }
