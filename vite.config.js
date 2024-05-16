7e8a79135fdd01373d80103df1639ff0bcbf1bfb
TyroneMartin <tyrone306@byui.edu>
2024-05-16 08:23:51 -0600

    Svelte Component added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
 
 export default defineConfig({
+  plugins: [svelte()],
   root: "src/",
-
   build: {
     outDir: "../dist",
     rollupOptions: {
@@ -12,9 +14,7 @@ export default defineConfig({
         cart: resolve(__dirname, "src/cart/index.html"),
         product: resolve(__dirname, "src/product_pages/index.html"
         ),
-        
       },
     },
   },
 });
-  
