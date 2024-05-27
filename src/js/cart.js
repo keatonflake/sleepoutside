7c5a41f87c57316cd9660ddf4d43d4e126d655be
TyroneMartin <tyrone306@byui.edu>
2024-05-27 11:53:22 -0600

    styling add, some path updated to rensure the relative path will be deployed.

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,8 +1,11 @@
 import ShoppingCart from "./components/ShoppingCart.svelte";
 import { loadHeaderFooter } from "./utils.mjs";
+// import { removeFromCart  } from "./productDetails.mjs";
+
 
 loadHeaderFooter();
 
 new ShoppingCart({
   target: document.querySelector(".products"),
+
 });
--- src/js/components/ShoppingCart.svelte
+++ src/js/components/ShoppingCart.svelte
@@ -1,8 +1,9 @@
 <script>
     import { getLocalStorage } from "../utils.mjs";
     const cartItems = getLocalStorage("so-cart") || [];
-  </script>
-  
+    console.log(cartItems);
+</script>
+
   <h2>Shopping Cart</h2>
   
 <div class="cart-container">
@@ -21,12 +22,13 @@
         <p class="cart-card__color">{item.Colors[0].ColorName}</p>
         <p class="cart-card__quantity blue"> Quantity: {item.Quantity} </p>
         <p class="cart-card__price blue">${(item.ListPrice * item.Quantity).toFixed(2)}</p>
-        <div class="cart-card__buttons">
-          <button class="cart-remove" onclick="removeFromCart(${item.Id})">Remove</button> 
-          <button class="cart-decrease" onclick="updateQuantity(${item.Id}, 'decrease')">➖</button>
-          <button class="cart-increase" onclick="updateQuantity(${item.Id}, 'increase')">➕</button>
-        </div>
+          <div class="cart-card__buttons">
+            <button class="cart-remove" id="removeFromCart" data-id="${item.Id}">Remove</button>
+            <button class="cart-decrease" id="decreaseQuantity" data-id="${item.Id}">➖</button>
+            <button class="cart-increase" id="increaseQuantity" data-id="${item.Id}">➕</button>
+          </div>
       </li>
     {/each}
   </ul>
 </div>
+
