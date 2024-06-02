3c5936e0a0f40eb3001024fed91672f50afc77e5
TyroneMartin <tyrone306@byui.edu>
2024-06-02 14:30:26 -0600

    Add customizable alert to added to index.html, and homepage styling updated

--- src/js/components/ProductList.svelte
+++ src/js/components/ProductList.svelte
@@ -1,44 +1,48 @@
 <script>
-    import { getData } from "../productData.mjs";    
+  import { getData } from "../productData.mjs";
 
-    // this is how we make a prop in svelte
-    export let category;
+  // this is how we make a prop in svelte
+  export let category;
 
-    let promise = getData(category);
-    console.log('Promise getData:', promise);
+  let promise = getData(category);
+  console.log("Promise getData:", promise);
 
-      // Function to capitalize the first letter
-    function capitalizeFirstLetter(string) {
+  // Function to capitalize the first letter
+  function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-    </script>
-    
+  }
+</script>
+
 <section class="products-container">
   <!-- <h2>Top Products: {category}</h2> -->
   <h2>Top Products: {capitalizeFirstLetter(category)}</h2>
 
-{#await promise}
-  <p>Loading data...</p>
-  <img class="loadingImage" src="../images/loading_image.gif" alt="loading page gif">
-{:then products}
-  <ul class="product-list-Home">
-    {#each products as product}
-      <li class="product-card">
-        <a href={`../../product_pages/index.html?product=${product.Id}`}>
+  {#await promise}
+    <p>Loading data...</p>
+    <img
+      class="loadingImage"
+      src="../images/loading_image.gif"
+      alt="loading page gif"
+    />
+  {:then products}
+    <ul class="product-list-Home">
+      {#each products as product}
+        <li class="product-card">
+          <a href={`../../product_pages/index.html?product=${product.Id}`}>
             <!-- <a href="../../product_pages/index.html"> -->
-          <img
-            src={product.Images.PrimaryMedium}
-            alt={product.Brand["Name"]} />
-          <h3 class="card__brand">{product.Brand["Name"]}</h3>
-          <h2 class="card__name">{product.NameWithoutBrand}</h2>
-          <p class="product-card__price">${product.ListPrice}</p>
-        </a>
-      </li>
-    {/each}
-  </ul>
-{:catch error}
-{console.log('Error:', error)}
-  <p>Error loading products: {error.message}</p>
-{/await}
-
+            <img
+              src={product.Images.PrimaryMedium}
+              alt={product.Brand["Name"]}
+            />
+            <h3 class="card__brand">{product.Brand["Name"]}</h3>
+            <h2 class="card__name">{product.NameWithoutBrand}</h2>
+            <p class="product-card__price">${product.ListPrice}</p>
+          </a>
+        </li>
+      {/each}
+    </ul>
+  {:catch error}
+    {console.log("Error:", error)}
+    <p>Error loading products: {error.message}</p>
+  {/await}
 </section>
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
 import { loadHeaderFooter} from "./utils.mjs";
+import loadAlerts from "./alerts.mjs";
 
 loadHeaderFooter();
+loadAlerts("alert-container");
+
+
+
+
+
 
